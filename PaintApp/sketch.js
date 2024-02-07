66a60967be0998d70b648ad3e34485028afed116
tina <113642406+ttina4@users.noreply.github.com>
2024-02-06 22:10:06 -0600

    finished

--- PaintApp/sketch.js
+++ PaintApp/sketch.js
@@ -1,66 +1,102 @@
+var paintbrush;
+/*var r = color("#ea412c");
+let o = color("#ef8734");
+let y = color("#fff84a");
+let g = color("#77f23b");
+let lb = color("#74f9fc");
+let db = color("#0044f7");
+let p = color("#e95dfa");
+let b = color("#774315");
+let w = color(255);
+let bl = color(0);*/
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(225);
+  paintbrush = color(0, 0, 0);
 }
 
-/*function windowResized() {
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-}*/
+}
 
-function draw() {
+function selection() {
+  strokeWeight(2);
+  stroke(255);
+  
+  fill(color("#ea412c"));
+  square(0, 0, 50);
 
-  noStroke();
+  fill(color("#ef8734"));
+  square(0, 50, 50);
 
-  let r = color("#ea412c");
-  let o = color("#ef8734");
-  let y = color("#fff84a");
-  let g = color("#77f23b");
-  let lb = color("#74f9fc");
-  let db = color("#0044f7");
-  let p = color("#e95dfa");
-  let b = color("#774315");
-  let w = color(255);
-  let bl = color(0);
-
-  fill(r);
-  let rsq = square(0, 0, 50);
-
-  fill(o);
-  let osq = square(0, 50, 50);
-
-  fill(y);
+  fill(color("#fff84a"));
   square(0, 100, 50);
 
-  fill(g);
+  fill(color("#77f23b"));
   square(0, 150, 50);
 
-  fill(lb);
+  fill(color("#74f9fc"));
   square(0, 200, 50);
 
-  fill(db);
+  fill(color("#0044f7"));
   square(0, 250, 50);
 
-  fill(p);
+  fill(color("#e95dfa"));
   square(0, 300, 50);
 
-  fill(b);
+  fill(color("#774315"));
   square(0, 350, 50);
 
-  fill(w);
+  fill(color(255));
   square(0, 400, 50);
 
-  fill(bl);
+  fill(color(0));
   square(0, 450, 50);
 
-  if (mouseIsPressed)
-  {
-    stroke(bl);
+}
+
+function mousePressed() {
+  if (mouseX < 50) {
+    if (mouseY < 50) {
+      paintbrush = color("#ea412c");
+    }
+    else if (mouseY < 100) {
+      paintbrush = color("#ef8734");
+    }
+    else if (mouseY < 150) {
+      paintbrush = color("#fff84a");
+    }
+    else if (mouseY < 200) {
+      paintbrush = color("#77f23b");
+    }
+    else if (mouseY < 250) {
+      paintbrush = color("#74f9fc");
+    }
+    else if (mouseY < 300) {
+      paintbrush = color("#0044f7");
+    }
+    else if (mouseY < 350) {
+      paintbrush = color("#e95dfa");
+    }
+    else if (mouseY < 400) {
+      paintbrush = color("#774315");
+    }
+    else if (mouseY < 450) {
+      paintbrush = color(255);
+    }
+    else if (mouseY < 500) {
+      paintbrush = color(0);
+    }
+  }
+}
+
+function draw() {
+  selection();
+
+  if (mouseIsPressed) {
+    stroke(paintbrush);
     strokeWeight(3);
     line(pmouseX, pmouseY, mouseX, mouseY);
   }
-  else if (rsq.mouseIsPressed)
-  {
-    stroke(r);
-  }
-
 }
