d228b8e04e5d2fc04e82e4079bea7b6de450f23d
tina <113642406+ttina4@users.noreply.github.com>
2024-02-23 23:18:28 -0600

    adjustments

--- BugSquish/sketch.js
+++ BugSquish/sketch.js
@@ -1,12 +1,10 @@
-let bgImage, score, walking, squished, time, gameState, startTime, timerIsDone, bugGroup;
+let score, walk, dead, time, gameState, startTime, timerIsDone, bugGroup;
 let walls = [];
 let bugs = [];
-let test;
 let dir = [0, 90, 180, 270];
 let speed = 2;
 
 function preload() {
-  //bgImage = loadImage("assets/background.jpg");
   bugs[0] = loadImage("assets/dead roach-1.png.png");
   bugs[1] = loadImage("assets/dead roach-2.png.png");
   bugs[2] = loadImage("assets/dead roach-3.png.png");
@@ -15,16 +13,16 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
   score = 0;
   startTime = 30;
   timerIsDone = false;
   gameState = "start";
   bugGroup = new Group();
   walls = new Group();
+  
   textFont('Comic Sans MS');
-  rectMode(CENTER);
   textAlign(CENTER);
-  borders();
 }
 
 function draw() {
@@ -32,47 +30,48 @@ function draw() {
 
   if (gameState == "start") {
     push();
-    fill(255);
-    fill(0);
     textSize(30);
-    text("squish the bugs\nclick to begin", width / 2, height / 2);
+    text("Squish roaches\n\nClick to start", width / 2, height / 2);
     pop();
+
     if (mouseIsPressed) {
       startTime = millis();
-      makeBugs(20);
+      makeBugs(10);
       gameState = "play";
     }
+
   } else if (gameState == "play") {
     push();
     time = timer();
-    fill(0)
     textSize(30);
     text("Time: " + (30 - time) + "\nScore: " + score, 100, 60);
     pop();
-    if(bugGroup.length < 29){
+
+    if(bugGroup.length < 19){
       makeBugs(10);
     }
     
     bugGroup.collide(walls, teleport);
     bugGroup.displace(bugGroup);
     drawSprites();
+
     if (time >= 30) {
       gameState = "end";
     }
+
   } else if (gameState == "end") {
     push();
-
-    fill(0);
     textSize(30);
-    text("Game Over!\nYou killed " + score + " bugs.\nPress space to play again!\n", width / 2, height / 2 - 20);
+    text("You killed " + score + " bugs.\nClick to play again.\n", width / 2, height / 2 - 20);
     pop();
 
-    if (keyIsPressed) {
+    if (mouseIsPressed) {
       startTime = millis();
       if (keyCode === 32) {
         setup();
       }
     }
+
   }
 }
 
@@ -84,34 +83,33 @@ function timer() {
 function makeBugs(num) {
   for (let i = 0; i < num; i++) {
 
-    let testBug = createSprite(random(100, width - 100), random(100, height - 100), 128, 128);
-    testBug.scale = 1;
-    testBug.isDead = false;
-    testBug.rotation = random(dir);
+    let roach = createSprite(random(100, width - 100), random(100, height - 100), 128, 128);
+    roach.isDead = false;
+    roach.rotation = random(dir);
 
-    if (testBug.rotation === 0) {
-      testBug.setSpeed(speed, 270);
-    } else if (testBug.rotation === 90) {
-      testBug.setSpeed(speed, 0);
-    } else if (testBug.rotation === 180) {
-      testBug.setSpeed(speed, 90);
-    } else if (testBug.rotation === 270) {
-      testBug.setSpeed(speed, 180);
+    if (roach.rotation === 0) {
+      roach.setSpeed(speed, 270);
+    } else if (roach.rotation === 90) {
+      roach.setSpeed(speed, 0);
+    } else if (roach.rotation === 180) {
+      roach.setSpeed(speed, 90);
+    } else if (roach.rotation === 270) {
+      roach.setSpeed(speed, 180);
     }
 
-    walking = testBug.addAnimation("walk",
+    walk = roach.addAnimation("walk",
       bugs[1],
       bugs[2]
     );
-    walking.frameDelay = 8;
+    walk.frameDelay = 8;
 
-    squished = testBug.addAnimation("squish",
+    dead = roach.addAnimation("dead",
       bugs[3]
     );
 
-    testBug.onMouseReleased = function() {
+    roach.onMouseReleased = function() {
       if (this.isDead === false) {
-        this.changeAnimation("squish");
+        this.changeAnimation("dead");
         this.setSpeed(0, 0);
         this.life = 100;
         score++;
@@ -119,7 +117,7 @@ function makeBugs(num) {
         this.isDead = true;
       }
     };
-    bugGroup.add(testBug);
+    bugGroup.add(roach);
   }
 }
 
