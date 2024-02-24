2423161294250621be2e6bb4683d7b5b6b7f1b1a
tina <mngu164@lsu.edu>
2024-02-23 19:52:22 -0600

    update

--- BugSquish/sketch.js
+++ BugSquish/sketch.js
@@ -1,60 +1,163 @@
-let bug;
+let bgImage, score, walking, squished, time, gameState, startTime, timerIsDone, bugGroup;
+let walls = [];
 let bugs = [];
-let time, score, startTime, endTime;
-let bugAni;
-let wall = [];
+let test;
+let dir = [0, 90, 180, 270];
+let speed = 2;
 
 function preload() {
-  bugs[0] = loadImage("dead roach-1.png");
-  bugs[1] = loadImage("dead roach-2.png");
-  bugs[2] = loadImage("dead roach-3.png");
-  bugs[3] = loadImage("dead roach-4.png");
+  //bgImage = loadImage("assets/background.jpg");
+  bugs[0] = loadImage("assets/dead roach-1.png");
+  bugs[1] = loadImage("assets/dead roach-2.png");
+  bugs[2] = loadImage("assets/dead roach-3.png");
+  bugs[3] = loadImage("assets/dead roach-4.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   score = 0;
-  startTime = 30; 
-  endTime = false;
-  time = 30;
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight)
+  startTime = 30;
+  timerIsDone = false;
+  gameState = "start";
+  bugGroup = new Group();
+  walls = new Group();
+  textFont('Comic Sans MS');
+  rectMode(CENTER);
+  textAlign(CENTER);
+  borders();
 }
 
 function draw() {
   background(220);
-  textSize(30);
-  text ("Time: " + time, 30, 50)
-  text("Score: ", 30, 100);
-  if (frameCount % 60 == 0 && time > 0) {
-    time--;
-  } else if (time == 0) {
-    text ("Gameover", windowWidth/2, windowHeight/2);
+
+  if (gameState == "start") {
+    push();
+    fill(255);
+    rect(width / 2, height / 2, 100, 75);
+    fill(0);
+    text("squish the bugs\nclick to begin", width / 2, height / 2);
+    pop();
+    if (mouseIsPressed) {
+      startTime = millis();
+      makeBugs(20);
+      gameState = "play";
+    }
+  } else if (gameState == "play") {
+    push();
+    time = timer();
+    fill(255)
+    textSize(width / 50);
+    text("time left: " + (30 - time) + "\nscore: " + score, 60, 30);
+    pop();
+    if(bugGroup.length < 1){
+      makeBugs(20);
+    }
+    
+    bugGroup.collide(walls, teleport);
+    bugGroup.displace(bugGroup);
+    drawSprites();
+    if (time >= 30) {
+      gameState = "end";
+    }
+  } else if (gameState == "end") {
+    push();
+    fill(255);
+    rect(width / 2, height / 2, 200, 100);
+    fill(0);
+    text("\ngame over!\nyou squished " + score + " bugs\npress space to play again\n", width / 2, height / 2 - 20);
+    pop();
+
+    if (keyIsPressed) {
+      startTime = millis();
+      if (keyCode === 32) {
+        setup();
+      }
+    }
   }
 }
 
+function timer() {
+  time = int((millis() - startTime) / 1000);
+  return time;
+}
 
-//roach still, roach move, roach die.
-//IDK why they collide nor do i know how to make walls plz help
-class roach{
-  constructor(x, y, width, height, spriteSheet, animation){
-    this.bug = new Sprite(x, y, width, height);
-    this.bug.spriteSheet = spriteSheet;
-    this.bug.anis.frameDelay = 2;
-    this.bug.addAnis(animation);
-    this.bug.changeAni('scuttle');
-    this.bug.vel.x = 3;
-    this.bug.vel.y = 0;
-    if (this.bug.x >= 600){
-      this.bug.vel.x = -3;
+function makeBugs(num) {
+  for (let i = 0; i < num; i++) {
+
+    let testBug = createSprite(random(100, width - 100), random(100, height - 100), 50, 50);
+    testBug.scale = 1;
+    testBug.isDead = false;
+    testBug.rotation = random(dir);
+
+    if (testBug.rotation === 0) {
+      testBug.setSpeed(speed, 270);
+    } else if (testBug.rotation === 90) {
+      testBug.setSpeed(speed, 0);
+    } else if (testBug.rotation === 180) {
+      testBug.setSpeed(speed, 90);
+    } else if (testBug.rotation === 270) {
+      testBug.setSpeed(speed, 180);
     }
+
+    walking = testBug.addAnimation("walk",
+      bugs[0],
+      bugs[1],
+    );
+    walking.frameDelay = 8;
+
+    squished = testBug.addAnimation("squish",
+      bugs[2]
+    );
+
+    testBug.onMouseReleased = function() {
+      if (this.isDead === false) {
+        this.changeAnimation("squish");
+        this.setSpeed(0, 0);
+        this.life = 100;
+        score++;
+        bugGroup.remove(this);
+        this.isDead = true;
+      }
+    };
+    bugGroup.add(testBug);
   }
-  squishBug(){
-    if(mouseButton.presses()){
-      this.bug.changeAni('squish');
+}
+
+function borders() {
+  for (let i = 0; i < 4; i++) {
+    let wall;
+    if (i === 0) {
+       wall = createSprite(width / 2, -100, 2000, 10);
+    } 
+    else if (i === 1) {
+     wall = createSprite(width / 2, height + 100, 2000, 10);
+    } 
+    else if (i === 2) {
+     wall = createSprite(-100, height / 2, 10, 2000);
+    } 
+    else if (i === 3) {
+     wall = createSprite(height + 100, height / 2, 10, 2000);
     }
+    wall.immovable = true;
+    walls.add(wall);
   }
- 
+}
+
+function teleport() {
+  if (this.rotation === 90) {
+    this.position.x = -50;
+    this.position.y = random(20, height - 20);
+  } 
+  else if (this.rotation === 270) {
+    this.position.x = width + 50;
+    this.position.y = random(20, height - 20);
+  } 
+  else if (this.rotation === 180) {
+    this.position.y = -50;
+    this.position.x = random(20, width - 20);
+  } 
+  else if (this.rotation === 0) {
+    this.position.y = height + 50;
+    this.position.x = random(20, width - 20);
+  }
 }
